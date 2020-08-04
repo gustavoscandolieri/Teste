48b670b61fdd8bb49ed88ead3281766233f9f4e0
Gustavo <killmorte@gmail.com>
2020-08-04 17:50:13 -0300

    5

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,28 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
     class Program
     {
-        public int Fibonacci(int n)
-        {
-            if (n == 2 || n == 3)
-                return 1;
-            else
-                return n;
-            
-            return Fibonacci(n-1) + Fibonacci(n-2);
-        }
-        
         static void Main(string[] args)
         {
             System.Console.WriteLine("Olá Mundo!");
 
+          //  System.Console.WriteLine("{0}", Fibonacci(4));
+
+            Fibonacci2(11);
+
             System.Console.ReadKey();
+
+
+        }
+
+        public static int Fibonacci(int n)
+        {
+            if (n < 2)
+                return n;
+            else
+                return  Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static void Fibonacci2(int N)
+        {
+            int i = 0;
+            int aux = 0, F0 = 0, F1 = 1; 
+
+            while (i < N)
+            {
+                System.Console.WriteLine("{0}", F0);
+                aux = F0 + F1;
+                F0 = F1;
+                F1 = aux;
+                i++;
+            }
         }
     }
 }
